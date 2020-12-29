df548c2cd40154409f28509fa2251df85dbd0d8c
Johann Perozo <johann.prz@gmail.com>
2020-12-29 01:09:20 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    background: path.join(__dirname, "src/background.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist/"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // Creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // Translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // Compiles Sass to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[hash]-[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[hash]-[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@src": path.resolve(__dirname, "src/"),
+    },
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+};
