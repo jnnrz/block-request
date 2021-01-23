c6e086f2a735707c11eb117758dbcffbab1b1aa4
Johann Perozo <johann.prz@gmail.com>
2021-01-23 01:13:21 -0400

    Adds css & js buttons, only js is active

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,15 @@
-import Toggle from "@src/components/toggle";
-import { BlockStatus } from "@src/types";
 import React, { FunctionComponent, useState, useEffect } from "react";
 import { browser } from "webextension-polyfill-ts";
+import { BlockStatus } from "@src/types";
+
+import styles from "./popup.scss";
 
 const Popup: FunctionComponent = () => {
   const [blockImages, setBlockImages] = useState<boolean>(false);
   const [blockMedia, setBlockMedia] = useState<boolean>(false);
   const [blockOther, setBlockOther] = useState<boolean>(false);
+  const [blockJs, setBlockJs] = useState<boolean>(false);
+  const [blockCss, setBlockCss] = useState<boolean>(false);
 
   useEffect(async () => {
     const { status } = await browser.storage.local.get("status");
@@ -14,6 +17,8 @@ const Popup: FunctionComponent = () => {
     setBlockImages(status.images);
     setBlockMedia(status.media);
     setBlockOther(status.other);
+    setBlockJs(status.js);
+    setBlockCss(status.css);
   }, []);
 
   const handleBlockImages = () => {
@@ -40,36 +45,74 @@ const Popup: FunctionComponent = () => {
     setBlockOther(!blockOther);
   };
 
+  const handleBlockJs = () => {
+    browser.runtime.sendMessage(
+      null,
+      blockJs ? BlockStatus.JS_UNBLOCK : BlockStatus.JS_BLOCK
+    );
+    setBlockJs(!blockJs);
+  };
+
+  const handleBlockCss = async () => {
+    await browser.runtime.sendMessage(
+      null,
+      blockCss ? BlockStatus.CSS_UNBLOCK : BlockStatus.CSS_BLOCK
+    );
+    setBlockCss(!blockCss);
+  };
+
   return (
-    <>
-      <div>
-        <label htmlFor="images">Images</label>
-        <input
-          type="checkbox"
-          name="images"
-          checked={blockImages}
-          onChange={handleBlockImages}
-        />
+    <div className={styles["wrapper"]}>
+      <div className={styles["control-wrapper"]}>
+        <div className={styles["control"]}>
+          <label htmlFor="images">Images</label>
+          <input
+            type="checkbox"
+            name="images"
+            checked={blockImages}
+            onChange={handleBlockImages}
+          />
+        </div>
+        <div className={styles["control"]}>
+          <label htmlFor="media">Media</label>
+          <input
+            type="checkbox"
+            name="media"
+            checked={blockMedia}
+            onChange={handleBlockMedia}
+          />
+        </div>
+        {/*<div className={styles["control"]}>
+          <label htmlFor="other">Other</label>
+          <input
+            type="checkbox"
+            name="other"
+            checked={blockOther}
+            onChange={handleBlockOther}
+          />
+        </div>*/}
+
+        <div className={styles["control"]}>
+          <label htmlFor="other">Javascript</label>
+          <input
+            type="checkbox"
+            name="other"
+            checked={blockJs}
+            onChange={handleBlockJs}
+          />
+        </div>
+
+        {/*<div className={styles["control"]}>
+          <label htmlFor="other">Css</label>
+          <input
+            type="checkbox"
+            name="other"
+            checked={blockCss}
+            onChange={handleBlockCss}
+          />
+        </div>*/}
       </div>
-      <div>
-        <label htmlFor="media">Media</label>
-        <input
-          type="checkbox"
-          name="media"
-          checked={blockMedia}
-          onChange={handleBlockMedia}
-        />
-      </div>
-      <div>
-        <label htmlFor="other">Other</label>
-        <input
-          type="checkbox"
-          name="other"
-          checked={blockOther}
-          onChange={handleBlockOther}
-        />
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/types.ts
+++ src/types.ts
@@ -5,4 +5,8 @@ export class BlockStatus {
   public static readonly MEDIA_UNBLOCK = 3;
   public static readonly OTHER_BLOCK = 4;
   public static readonly OTHER_UNBLOCK = 5;
+  public static readonly JS_BLOCK = 6;
+  public static readonly JS_UNBLOCK = 7;
+  public static readonly CSS_BLOCK = 8;
+  public static readonly CSS_UNBLOCK = 9;
 }
